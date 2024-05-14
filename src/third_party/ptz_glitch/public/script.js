a39c231ec1a08e12837c7de259df8f3633442120
Jason Novak <jason@nvkmail.com>
2024-05-13 21:03:55 -0700

    remove getUserMediaVideoPtzButton

--- src/third_party/ptz_glitch/public/script.js
+++ src/third_party/ptz_glitch/public/script.js
@@ -90,8 +90,6 @@ window.addEventListener("DOMContentLoaded",
                         { video: { pan: true, tilt: true, zoom: true } }));
 
 getUserMediaVideoButton.onclick = (_) => getUserMedia({ video: true });
-getUserMediaVideoPtzButton.onclick = (_) =>
-  getUserMedia({ video: { zoom: 400 } });
 getUserMediaVideoPanUnconstrainedAudioButton.onclick = (_) =>
   getUserMedia({ audio: true, video: { tilt: true } });
 
